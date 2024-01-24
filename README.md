8fe31edc2f00f9df804f4abae13150daadcc01eb
Your Name <nikhanks@gmail.com>
2024-01-24 12:26:08 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Overview
+This code utilizes database management methods through a python extension called SQLite3 to manipulate data stored in a database. It can create a data table that is modifiable and able to execute commands such as, insert, delete, and retrieve(or query) data. It can also submit commands as well as receive them. 
+
+{Important! Do not say in this section that this is college assignment. Talk about what you are trying to accomplish as a software engineer to further your learning.}
+
+{Provide a description of the software that you wrote and how it integrates with a SQL Relational Database. Describe how to use your program.} *
+
+The purpose of this software is to utilize the SQLite3 module. 
+{Describe your purpose for writing this software.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running, a walkthrough of the code, and a view of how you created the Relational Database.}
+
+# Relational Database
+
+{Describe the relational database you are using.}
+The database I am using is the 
+
+{Describe the structure (tables) of the relational database that you created.}
+The data structure of this type of data base is: 
+
+# Development Environment
+
+{Describe the tools that you used to develop the software}
+The tools that I've needed to develop this software is
+
+{Describe the programming language that you used and any libraries.}
+the programming language that I used is X with the libraries: x, y, z. 
+
+# Useful Websites
+
+
+- [Python sqlite3](https://docs.python.org/3.8/library/sqlite3.html#)
+- [SQLite Tutorial](https://www.sqlitetutorial.net/)
+- [TutorialsPoint: SQLite - Python](https://www.tutorialspoint.com/sqlite/sqlite_python.htm)
+
+
+# Future Work
+
+- Add additional tables to your database and perform a join in your software between two of the tables.
+- Use at least two of the aggregate functions to summarize numerical data in your database.
+- Demonstrate the use of a column containing a date or time along with a query that demonstrates filtering within a date or time range.
---
+++ main.py
@@ -0,0 +1,92 @@
+"""
+SQL Database Workshop
+
+This Database will store employee name, contact details, and department. 
+"""
+
+import sqlite3
+
+def analyze_object(object):
+    print(f"Object: {type(object)}")
+    for name in dir(object):
+        if not name.startswith("__"):
+            attribute = getattr(object,name)    
+            if callable(attribute):
+                print(f"Function: {name}")
+            else:
+                print(f"{name} = {attribute}")
+    print()
+
+# Connect to the database
+connection = sqlite3.connect('records.db')
+cursor = connection.cursor()
+
+# Create table (if it does not already exist)
+cursor.execute("CREATE TABLE IF NOT EXISTS employees (name TEXT, title TEXT, pay REAL)")
+       
+def get_name(cursor):
+    cursor.execute("SELECT name FROM employees")
+    results = cursor.fetchall()
+    if len(results) == 0:
+        print("No names in database")
+        return None
+    for i in range(len(results)):
+        print(f"{i+1} - {results[i][0]}")
+    choice = 0
+    while choice < 1 or choice > len(results):
+        choice = int(input("Name ID: "))
+    return results[choice-1][0]
+
+
+choice = None
+while choice != "5":
+    print("1) Display Employees")
+    print("2) Add Employee")
+    print("3) Update Employee Pay")
+    print("4) Delete Employee")
+    print("5) Quit")
+    choice = input("> ")
+    print()
+    if choice == "1":
+        # Display Employees
+        result = cursor.execute("SELECT * FROM employees ORDER BY pay DESC")
+        # analyze_object(result)
+        print("{:>10}  {:>10}  {:>10}".format("Name", "Title", "Pay"))
+        for record in cursor.fetchall():
+            print("{:>10}  {:>10}  {:>10}".format(record[0], record[1], record[2]))
+    elif choice == "2":
+        # Add New Employee
+        try:
+            name = input("Name: ")
+            title = input("Title: ")
+            pay = float(input("Pay: "))
+            values = (name, title, pay)
+            cursor.execute("INSERT INTO employees VALUES (?,?,?)", values)
+            connection.commit()
+        except ValueError:
+            print("Invalid pay!")
+    elif choice == "3":
+        # Update Employee Pay
+        try:
+            name = input("Name: ")
+            pay = float(input("Pay: "))
+            values = (pay, name) # Make sure order is correct
+            cursor.execute("UPDATE employees SET pay = ? WHERE name = ?", values)
+            connection.commit()
+            # analyze_object(result)
+            if cursor.rowcount == 0:
+                print("Invalid name!")
+        except ValueError:
+            print("Invalid pay!")
+    elif choice == "4":
+        # Delete employee
+        name = get_name(cursor)
+        if name == None:
+            continue
+        values = (name, )
+        cursor.execute("DELETE FROM employees WHERE name = ?", values)
+        connection.commit()
+    print()
+
+# Close the database connection before exiting
+connection.close()
